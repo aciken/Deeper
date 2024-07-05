876d5547eb51aad24cf3775d5445c4c3be56f2ea
aciken <adrianmarton2006@gmail.com>
2024-07-05 21:11:37 +0200

    first

---
+++ FrontEnd/app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="h-full bg-gray-800 justify-center items-center">
+      <Text className="text-2xl font-pblack">index</Text>
+    </View>
+  )
+}
+
+export default index
